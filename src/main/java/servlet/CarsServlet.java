aa91ff024b0c94e7289d9c6c0a8fae6b83740109
Mikołaj Krawczak <mikolak25@gmail.com>
2019-11-07 18:47:15 +0100

    Added frontend

--- src/main/java/servlet/CarsServlet.java
+++ src/main/java/servlet/CarsServlet.java
@@ -1,8 +1,36 @@
 package servlet;
 
+import freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/cars")
-public class CarsServlet {
+public class CarsServlet extends HttpServlet {
 
+  @Inject
+  TemplateProvider templateProvider;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    Template template = templateProvider.getTemplate(getServletContext(), "cars-site.ftlh");
+    PrintWriter printWriter = resp.getWriter();
+    Map<String, String> dataModel = new HashMap<>();
+
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
 }
